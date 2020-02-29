441e4ad3cc5fd82a390d59049d711fd93468e741
s16578 <s16578@pjwstk.edu.pl>
2020-02-29 21:24:50 +0100

    App

--- Cwiczenia1/Cwiczenia1/Program.cs
+++ Cwiczenia1/Cwiczenia1/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace Cwiczenia1
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var url = args.Length > 0 ? args[0] : "https://www.pja.edu.pl";
+            var client = new HttpClient();
+            var result = await client.GetAsync(url);
+
+            if (result.IsSuccessStatusCode) //2xx
+            {
+                string html = await result.Content.ReadAsStringAsync();
+                var regex = new Regex("[a-z]+[a-z0-9]*@[a-z0-9]+\\.[a-z]+",RegexOptions.IgnoreCase);
+
+
+                MatchCollection matches = regex.Matches(html);
+                foreach(var m in matches)
+                {
+                    Console.WriteLine(m);
+                }
+            }
+            
+            //JS - promise async await
+            //Java - Future<T>
+            //Task -> async await
+
+            Console.WriteLine("Do zobaczenia!");
         }
     }
 }
